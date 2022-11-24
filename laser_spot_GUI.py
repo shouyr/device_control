e8b7de49b512366aa8d9d4be6f9daf94e7f97815
Shou <shouyinren@163.com>
2022-11-24 14:55:24 +0900

    add result save

--- laser_spot_GUI.py
+++ laser_spot_GUI.py
@@ -44,7 +44,10 @@ def CaptureScreen():
     y1=y0+640
     im=ImageGrab.grab((x0,y0,x1,y1))
     im.save(path_[0:-4]+'.jpg','jpeg')
-    
+    np.savetxt(path_[0:-4]+'.txt',(x,y),header='first line x in unit um, second line concentration',fmt='%.4f')
+    with open("result.txt", "ab") as f:
+        np.savetxt(f, resout.T,fmt='%.3e', header=path_+', 1/e^2 a,'+' b,'+' FWHM a,'+' b,'+' sqrt(ab),'+' concentration,'+' intensity',newline='  ', footer='\n',comments='')
+        
 # you can use other keys and replace it with "<Return>". EX: "f"
 # by default, this function will pass an unknown argument to your function.
 # thus, you have to give your function a parameter; in this case, we will use *args
@@ -61,6 +64,10 @@ def refresh(*args):
     background = np.float64(background)
     img_open = Image.open(path_)
     image = np.array(img_open)
+    maxpixel = inputa.get()
+    maxpixel = np.float64(maxpixel)
+    minpixel = inputb.get()
+    minpixel = np.float64(minpixel)
     # convert to unit8
     tracking = image/np.max(image)*255
     tracking = tracking.astype(np.uint8)
@@ -69,7 +76,8 @@ def refresh(*args):
     gmx = 253
     #apply thresholding to grayscale frames. 
     ret, thresh = cv2.threshold(tracking, gmn, gmx, 0)
-    
+    global resout
+    resout = np.zeros(7)
     # find contours in the threshold image
     contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_TC89_L1)
     
@@ -139,12 +147,16 @@ def refresh(*args):
     cone2 = (sum0-sum1)/(sum0-sumr[-1]) 
     # img = ImageTk.PhotoImage(img_open.resize((500,500)))
     # spot1 = np.resize(spot1,(500,500))
-    img = Image.fromarray(spot1)
+    spot1[spot1>maxpixel] = maxpixel
+    spot1[spot1<minpixel] = minpixel
+    spot1 = (spot1-minpixel)/(np.max(spot1)-minpixel)*255
+    img = Image.fromarray(np.uint8(spot1), mode="L")
     img = img.resize((500,500))
     img = ImageTk.PhotoImage(img)
     lableShowImage1.config(image=img)
     lableShowImage1.image = img
-    out1.set('1/e^2 a='+str(np.round(a*num,2))+' um  b='+str(np.round(b*num,2)) + ' um,'+' concentration='+str(np.round(cone2*100,2))+'%')
+    out1.set('1/e^2 a='+str(np.round(a*num,2))+' b='+str(np.round(b*num,2)) + ' '+'√ab='+str(np.round(np.sqrt(a*b)*num,2)) + ' um,'+' concentration='+str(np.round(cone2*100,2))+'%')
+    resout[0:2]=[a*num,b*num]
 
     tracking3 = image/np.max(image[xc-5:xc+5,yc-5:yc+5])*255
     tracking3 = tracking3.astype(np.uint8)
@@ -174,10 +186,11 @@ def refresh(*args):
     cv2.ellipse(spot0,ellipse,(0,0,0),-1)
     sum1 = np.sum(spot0[xc-rmax:xc+rmax,yc-rmax:yc+rmax])
     confwhm = (sum0-sum1)/(sum0-sumr[-1]) 
-    out2.set('FWHM a='+str(np.round(a*num,2))+' um  b='+str(np.round(b*num,2)) + ' um,'+' concentration='+str(np.round(confwhm*100,2))+'%')
+    out2.set('FWHM a='+str(np.round(a*num,2))+' b='+str(np.round(b*num,2)) + ' '+'√ab='+str(np.round(np.sqrt(a*b)*num,2)) + ' um,'+' concentration='+str(np.round(confwhm*100,2))+'%')
     intensity = 4*np.log(2)*laser_energy/np.pi/laser_t*1e15/a/b/num/num*1e8*confwhm/0.5
     out3.set('intensity = ' + str(format(intensity, '.2e')) + 'W/cm^2')
-
+    resout[2:7]=[a*num,b*num,np.sqrt(a*b)*num,confwhm,intensity]
+    
     global x ,y
     x = r*num
     y = con
@@ -204,59 +217,71 @@ if __name__ == '__main__':
     #Entry widget which allows displaying simple text.
     path = tk.StringVar()
     entry = tk.Entry(app, state='readonly', text=path,width = 100)
-    entry.grid(columnspan=4)
+    entry.grid(columnspan=6)
     lableShowImage1 = tk.Label(app)
-    lableShowImage1.grid(row=1,column=0,columnspan=2)
+    lableShowImage1.grid(row=1,column=0,columnspan=3)
     canvas = FigureCanvasTkAgg(f,master=app)
     canvas.draw()
-    canvas.get_tk_widget().grid(row=1,column=2,columnspan=2)
-    buttonSelImage = tk.Button(app, text='open tiff', command=choosepic)
+    canvas.get_tk_widget().grid(row=1,column=3,columnspan=3)
+    buttonSelImage = tk.Button(app, text='open tiff image', command=choosepic)
     buttonSelImage.grid(row=2,column=0)
-    buttonCapture = tk.Button(app, text='save result', command=CaptureScreen)
-    buttonCapture.grid(row=2,column=1)
+    buttonCapture = tk.Button(app, text='save the result', command=CaptureScreen)
+    buttonCapture.grid(row=2,column=5)
     #buttonSelImage.pack(side=tk.BOTTOM)
     #Call the mainloop of Tk.
-    label1 = tk.Label(app, text="um/pixel")
+    label1 = tk.Label(app, text="calibration um/pixel")
     label1.grid(row=3,column=0)
     input1 = tk.StringVar(value = '0.1315')
     entry1 = tk.Entry(app,text = input1)
     entry1.bind('<Return>',refresh)
-    entry1.grid(row=4,column=0)
+    entry1.grid(row=3,column=1)
     out1 = tk.StringVar()
-    entry2 = tk.Entry(app, state='readonly', text=out1,width = 50)
-    entry2.grid(row=5,column=0,columnspan=2)
+    entry2 = tk.Entry(app, state='readonly', text=out1,width = 60)
+    entry2.grid(row=5,column=0,columnspan=4)
+    labela = tk.Label(app, text="                          Max pixel",anchor='e')
+    labela.grid(row=2,column=3)
+    inputa = tk.StringVar(value = '200')
+    entrya = tk.Entry(app,text = inputa)
+    entrya.bind('<Return>',refresh)
+    entrya.grid(row=2,column=4)
+    labelb = tk.Label(app, text="                          Min pixel",anchor='e')
+    labelb.grid(row=2,column=1)
+    inputb = tk.StringVar(value = '0')
+    entryb = tk.Entry(app,text = inputb)
+    entryb.bind('<Return>',refresh)   
+    entryb.grid(row=2,column=2)
     input2 = tk.StringVar(value = '60')
     entry3 = tk.Entry(app,text = input2)
     entry3.bind('<Return>',refresh)
-    entry3.grid(row=4,column=1)
+    entry3.grid(row=3,column=3)
     label2 = tk.Label(app, text="plot_size_um")
-    label2.grid(row=3,column=1)
+    label2.grid(row=3,column=2)
     out2 = tk.StringVar()
-    entry4 = tk.Entry(app, state='readonly', text=out2,width = 50)
-    entry4.grid(row=5,column=2,columnspan=2)
+    entry4 = tk.Entry(app, state='readonly', text=out2,width = 60)
+    entry4.grid(row=6,column=0,columnspan=4)
     input3 = tk.StringVar(value = '20')
     entry5 = tk.Entry(app,text = input3)
     entry5.bind('<Return>',refresh)
-    entry5.grid(row=4,column=2)
+    entry5.grid(row=4,column=4)
     label3 = tk.Label(app, text="laser_energy_J")
-    label3.grid(row=3,column=2)
+    label3.grid(row=3,column=4)
     input4 = tk.StringVar(value = '26')
     entry6 = tk.Entry(app,text = input4)
     entry6.bind('<Return>',refresh)
-    entry6.grid(row=4,column=3)
+    entry6.grid(row=4,column=5)
     label4 = tk.Label(app, text="laser_duration_fs")
-    label4.grid(row=3,column=3)
+    label4.grid(row=3,column=5)
     out3 = tk.StringVar()
-    entry7 = tk.Entry(app, state='readonly', text=out3,width = 50)
-    entry7.grid(row=2,column=2,columnspan=2)
+    entry7 = tk.Entry(app, state='readonly', text=out3,width = 30,font=("Arial 12"))
+    entry7.grid(row=5,column=4,columnspan=2,rowspan=2)
     input5 = tk.StringVar(value = '0')
     entry8 = tk.Entry(app,text = input5)
     entry8.bind('<Return>',refresh)
-    entry8.grid(row=6,column=3)
+    entry8.grid(row=4,column=3)
     label4 = tk.Label(app, text="set_background")
-    label4.grid(row=6,column=2)
+    label4.grid(row=4,column=2)
     out4 = tk.StringVar()
-    entry9 = tk.Entry(app, state='readonly', text=out4,width = 50)
-    entry9.grid(row=6,column=0,columnspan=2)
+    entry9 = tk.Entry(app, state='readonly', text=out4,width = 30)
+    entry9.grid(row=4,column=0,columnspan=2)
     app.mainloop()
     
